bbab8d349e438aebcae5fc523ed90c1c14e42203
trohith89 <75958140+trohith89@users.noreply.github.com>
2023-06-13 19:26:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 //your code here
 
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+
+let compare = (a,b) => {
+	return a-b;
+}
+
+touristSpots.sort(compare);
+console.log(touristSpots);
